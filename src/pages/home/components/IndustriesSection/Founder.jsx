5986cb1039c4f3e33684d27e854ac51d77bde2be
Ajmal Fariz <ajfariz13@gmail.com>
2024-06-20 18:37:00 +0530

    founder section done

--- src/pages/home/components/IndustriesSection/Founder.jsx
+++ src/pages/home/components/IndustriesSection/Founder.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
-
+import quoteleft from '../../assets/quote-left.png'
+import quoteright from '../../assets/quote-right.png'
 const Founder = () => {
 const Skywalks = {
     fontFamily : 'Skywalks',
@@ -25,11 +26,13 @@ const Lato = {
         <div className='px-8 w-full md:w-[70%] lg:w-[40%] flex items-center justify-center'>
         <img  src="https://www.entrepreneurcafe.org/uploads/2/5/4/7/25478136/12_5_orig.jpg" alt="" srcset="" />
         </div>
-          <div className='px-6 py-4 w-full lg:w-[60%] lg:mb-10 flex flex-col items-center bg-[#fddb60] rounded-xl'>
-               <p style={Lato} className='text-base md:text-lg'>
+          <div className='relative px-10 py-8 w-full lg:w-[60%] lg:mb-10 flex flex-col items-center bg-[#fddb60] rounded-xl'>
+            <img className='absolute top-4 left-4 w-6' src={quoteleft} alt="" srcset="" />
+            <img className='absolute bottom-4 right-4 w-6' src={quoteright} alt="" srcset="" />
+               <p style={Lato} className='p-2 mt-2 text-base md:text-lg'>
                Lorem ipsum dolor sit amet consectetur. Facilisi elit nam id nulla phasellus ac elementum ullamcorper posuere. Amet sollicitudin phasellus arcu ante justo mauris id. Massa eget viverra varius tellus neque. Id ut elementum pellentesque vel in. In sodales facilisis vitae magnis sed convallis. Elit quis in phasellus cum nec. A est in sapien ullamcorper purus facilisi. Adipiscing amet platea tempor nibh.
                </p>
-               <div className='w-fit p-2  lg:text-2xl bg-white mt-8'>
+               <div className='w-fit p-2  lg:text-2xl bg-white mt-2'>
                    <p style={Skywalks}>Mr. Binod Kumar Bawri</p>
                </div>
           </div>
