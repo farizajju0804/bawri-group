32cd5a2809b54f07b49cd4a01f69318202c775d2
Ajmal Fariz <ajfariz13@gmail.com>
2024-06-25 17:16:25 +0530

    hero changes

--- src/pages/home/components/IndustriesSection/HeroVideo.jsx
+++ src/pages/home/components/IndustriesSection/HeroVideo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 const HeroVideo = () => {
+    const isMedium = window.innerWidth < 1024
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center">
       <div className="absolute inset-0 overflow-hidden">
@@ -11,7 +12,7 @@ const HeroVideo = () => {
           preload="auto"
           muted 
           className="absolute inset-0 w-full h-full object-cover object-right"
-          src='https://res.cloudinary.com/drlyyxqh9/video/upload/v1719314058/Bawri%20Group/Landing%20Page/bg._tetn2e.webm'
+          src={isMedium ? 'https://video-previews.elements.envatousercontent.com/bc8e7f57-c8e0-447c-818e-e3cc3d71d670/watermarked_preview/watermarked_preview.mp4' :'https://res.cloudinary.com/drlyyxqh9/video/upload/v1719314058/Bawri%20Group/Landing%20Page/bg._tetn2e.webm'}
         >
         </video>
       </div>
