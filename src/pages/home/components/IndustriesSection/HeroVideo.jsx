f3ab12c4ba63044f02ed16648771100641fee999
Ajmal Fariz <ajfariz13@gmail.com>
2024-06-25 17:06:44 +0530

    hero mp4

--- src/pages/home/components/IndustriesSection/HeroVideo.jsx
+++ src/pages/home/components/IndustriesSection/HeroVideo.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const HeroVideo = () => {
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center">
@@ -10,9 +9,8 @@ const HeroVideo = () => {
           loop 
           muted 
           className="absolute inset-0 w-full h-full object-cover object-right"
-          src='https://res.cloudinary.com/drlyyxqh9/video/upload/v1719314058/Bawri%20Group/Landing%20Page/bg._tetn2e.webm'
+          src='https://res.cloudinary.com/drlyyxqh9/video/upload/v1719311430/Bawri%20Group/Landing%20Page/background-video.mp4'
         >
-          {/* <source src="https://video-previews.elements.envatousercontent.com/bc8e7f57-c8e0-447c-818e-e3cc3d71d670/watermarked_preview/watermarked_preview.mp4" type="video/mp4" /> */}
         </video>
       </div>
       <div className="relative z-10 text-center text-white">
